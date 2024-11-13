ec2ca233e53d83b4097e7e138ac069f8b5b1997c
mishadcf <misha.freidin@gmail.com>
2024-11-13 12:24:44 +0000

    trims fat, adds more accurate function name

--- src/data_preprocessing.py
+++ src/data_preprocessing.py
@@ -196,48 +196,12 @@ def concat_all_river_gauges(river_directory="data/river_data"):
 
 
 
-def check_missing_days_in_csv(file_path):
-    """Check a single CSV for missing days in the river gauge data and calculate the percentage of missing days."""
-    # Read the CSV file
-    river_data = pd.read_csv(file_path)
-    
-    # Convert the 'time' column to datetime (adjust column name if needed)
-    river_data['time'] = pd.to_datetime(river_data['time'])
-    
-    # Resample the data to daily frequency
-    river_data_daily = river_data.set_index('time').resample('D').asfreq()
-    
-    # Find missing days
-    missing_days = river_data_daily[river_data_daily.isnull().any(axis=1)].index
-    
-    # Calculate total number of days and percentage of missing days
-    total_days = len(river_data_daily)
-    missing_percentage = (len(missing_days) / total_days) * 100 if total_days > 0 else 0
-    
-    return missing_days, missing_percentage
-
-def check_missing_days_in_directory(directory):
-    """Check all CSV files in a directory for missing days and calculate their percentages."""
-    csv_files = [f for f in os.listdir(directory) if f.endswith('.csv')]
-    files_with_missing_days = {}
-    
-    for file_name in csv_files:
-        file_path = os.path.join(directory, file_name)
-        missing_days, missing_percentage = check_missing_days_in_csv(file_path)
-        
-        if len(missing_days) > 0:
-            files_with_missing_days[file_name] = {
-                'num_missing_days': len(missing_days),
-                'missing_percentage': missing_percentage
-            }
-    
-    return files_with_missing_days
 
 def remove_negative_river_levels(df):
     df['value'] =  df['value'].apply(lambda x : np.nan if x<0 else x)
     return df
 
-def count_missing_quarter_hour_rows(df, filename):
+def count_missing_quarter_hour_rows_river(df, filename):
     
     df = df.reset_index()
     # Ensure the timestamp column is in datetime format and set as index
